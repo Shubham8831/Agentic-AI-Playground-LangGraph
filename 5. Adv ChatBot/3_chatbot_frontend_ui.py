dd6a943fdaa342ac0b138a1ad86fca7c336cad24
Shubham8831 <shubuamsingh2002@gmail.com>
2025-08-11 23:05:43 +0530

    streaming feature added in chatbot

--- 5. Adv ChatBot/3_chatbot_frontend_ui.py
+++ 5. Adv ChatBot/3_chatbot_frontend_ui.py
@@ -2,21 +2,23 @@ import streamlit as st
 from chatbot_backend_2 import chatbot
 from langchain_core.messages import HumanMessage
 
-# st.session_state -> dict -> 
 CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
+
+# st.session_state -> dict ->  special type of dictionary that does not erase its content when we press enter it only erases when we refresh page manually
+#if not then add a enpty list
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
 # loading the conversation history
 for message in st.session_state['message_history']:
-    with st.chat_message(message['role']):
+    with st.chat_message(message['role']): #loading role
         st.text(message['content'])
 
 #{'role': 'user', 'content': 'Hi'}
 #{'role': 'assistant', 'content': 'Hi=ello'}
 
-user_input = st.chat_input('Type here')
+user_input = st.chat_input('Type here...')
 
 if user_input:
 
@@ -25,7 +27,27 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    # first add the message to message_history
+    # response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    
+    # ai_message = response['messages'][-1].content 
+    # # first add the message to message_history
+    # st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    # with st.chat_message('assistant'):
+    #     st.text(ai_message)
+
+
+    # THE ABOVE INVOKE WAY TO PROVIDE USER MESSAGES IS BAD AS IT FIRST GENERATES THE RESPONSE AND THEN IT PRINTS ALL AT ONCE
+
+    # STREAMING - to get token by token output form the llm (the models start sending tokens (messages) 
+    # as soon as they are generated instead of wating for entire response to be reaby before returning it)
+
+    #so rather then using .invoke we use .stream -> this returns a generator object -> 
+    # -> generator object is a special type of iterator that allows you to generate values one at a time, using yield keyword instead of return
+
+
+  #  st.write_stream : write generator or streams to the app with a typewriter effect
+   
+    # STREAMING IMPLEMENTATION : 
     with st.chat_message('assistant'):
 
         ai_message = st.write_stream(
@@ -36,4 +58,4 @@ if user_input:
             )
         )
 
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message}) # SAVING THE OUTPUT OF ASSISTANT
--- 5. Adv ChatBot/chatbot_backend_2.py
+++ 5. Adv ChatBot/chatbot_backend_2.py
@@ -5,6 +5,7 @@ from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
 import os
 load_dotenv()
 key = os.getenv("GROQ_API_KEY")
@@ -27,3 +28,16 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+# for message_chunk, metadata in chatbot.stream(
+#     {"messages":[HumanMessage(content = "what is recipi to make pasta")]},  # first we pass the initial state
+#     CONFIG = {'configurable': {'thread_id': 'thread-1'}}, # then we pass the config for thread id
+#     stream_mode="messages" # modes for streaming
+# ):
+
+# # stream will give a generator object
+# # print(type(stream))
+
+# #stream object has message chunk and metadata 
+#      if message_chunk.content:
+#          print(message_chunk.content, end="", flush=True)
